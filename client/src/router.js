92db1415cc4eea5ae7a0e8d2173f4eda8263358c
Tyler Coats <coatsctyler@gmail.com>
2023-03-16 15:23:07 -0400

    Added course route

--- client/src/router.js
+++ client/src/router.js
@@ -2,10 +2,13 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import HomePage from '../src/pages/HomePage.vue';
 import StudentPage from './pages/StudentPage.vue';
+import CoursePage from './pages/CoursePage.vue';
 
 const routes = [
     {path: '/', component: HomePage, name: 'HomePage'},
-    {path: '/students', component: StudentPage, name: 'StudentPage'}
+    {path: '/students', component: StudentPage, name: 'StudentPage'},
+    {path: '/courses', component: CoursePage, name: 'CoursePage'}
+    
 ]
 
 
