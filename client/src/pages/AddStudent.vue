4f71c4f0307e292c9c76dfe34e5b74725e7b8db1
jonclindner <jonathan.c.lindner@gmail.com>
2023-03-16 13:28:39 -0600

    added form pages

---
+++ client/src/pages/AddStudent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="">
+      <form @submit="">
+        <input :value="" @input="" />
+        <button>Add Student to Database</button>
+      </form>
+   
+        
+      
+    </div>
+    
+    </div>
+    </template>
+    
+ 
+    
+  
+    
+    <script>
+        export default { 
+            name: 'AddStudent',
+        }
+    </script>
+    
+    
+    <style>
+    
+    </style>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        Home Page Banner
+        CoursePal
     </div>
     <div>
         Home Page Feature Section
