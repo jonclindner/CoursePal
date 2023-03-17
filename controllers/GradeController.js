aac4c2f7f06b2bfa7103400e808fff58d6870f7c
NellyNel520 <channelharris520@gmail.com>
2023-03-17 00:34:32 -0400

    update grade controller and route successful

--- controllers/GradeController.js
+++ controllers/GradeController.js
@@ -20,8 +20,22 @@ const GetGrades = async (req, res) => {
   }
 }
 
+const UpdateGrade = async (req, res) => {
+  try {
+    let gradeId = req.params.grade_id
+    let updatedGrade = await Grade.update(req.body, {
+      where: { id: gradeId },
+      returning: true
+    })
+    res.send(updatedGrade)
+  } catch (error) {
+    throw error
+  }
+}
+
 
 module.exports = {
   CreateGrade,
-  GetGrades
+  GetGrades,
+  UpdateGrade
 }
--- routes/GradeRouter.js
+++ routes/GradeRouter.js
@@ -3,6 +3,7 @@ const controller = require('../controllers/GradeController')
 
 Router.post('/createGrade', controller.CreateGrade)
 Router.get('/', controller.GetGrades)
+Router.put('/:grade_id', controller.UpdateGrade)
 
 
 module.exports = Router
