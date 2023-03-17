cdc53ace80b103b763749c768566eeaae60f88bb
Tyler Coats <coatsctyler@gmail.com>
2023-03-17 11:14:16 -0400

    Added course axios call

--- client/src/pages/CoursePage.vue
+++ client/src/pages/CoursePage.vue
@@ -17,30 +17,26 @@
     
 <script>
 import ViewCourses from '../components/ViewCourses.vue'
+import axios from 'axios'
 export default {
     name: 'CoursePage',
     components: {
         ViewCourses
     },
     data: () => ({
-        courses: [{
-            id: 101,
-            name: 'BIO',
-        }, {
-            id: 220,
-            name: 'LIT',
-        }, {
-            id: 309,
-            name: 'MATH',
-        },{
-            id: 619,
-            name: 'SCI',
-        }
-        ]
+        courses: []
     }),
+    mounted() {
+        this.getCourses()
+    },
     methods: {
         navigateCourse(id) {
             this.$router.push(`/courses/${id}`)
+        },
+        async getCourses() {
+            const response = await axios.get(`http://localhost:3001/api/course/`)
+            console.log(response);
+            this.courses = response.data
         }
     }
 }
