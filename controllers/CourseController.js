167318ee73f01c295288dae6d43cb007e343b00c
NellyNel520 <channelharris520@gmail.com>
2023-03-16 23:29:58 -0400

    delete courses controller and route successful

--- controllers/CourseController.js
+++ controllers/CourseController.js
@@ -33,9 +33,22 @@ const UpdateCourse = async (req, res) => {
   }
 }
 
+const DeleteCourse = async (req, res) => {
+  try {
+    let courseId = req.params.course_id
+    await Course.destroy({ 
+      where: { id: courseId }
+     })
+    res.send({ message: `Deleted Student with an ID of ${courseId}!` })
+  } catch (error) {
+    throw error
+  }
+}
+
 module.exports = {
   CreateCourse,
   GetCourses,
-  UpdateCourse
+  UpdateCourse,
+  DeleteCourse
 
 }
--- routes/CourseRouter.js
+++ routes/CourseRouter.js
@@ -5,5 +5,6 @@ const controller = require('../controllers/CourseController')
 Router.post('/createCourse', controller.CreateCourse)
 Router.get('/', controller.GetCourses)
 Router.put('/:course_id', controller.UpdateCourse)
+Router.delete('/:course_id', controller.DeleteCourse)
 
 module.exports = Router
