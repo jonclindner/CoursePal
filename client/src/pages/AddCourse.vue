4a5f93514cc9018e282df2e7fc2a5885b88b0d63
jonclindner <jonathan.c.lindner@gmail.com>
2023-03-17 08:07:00 -0600

    installed axios

--- client/src/pages/AddCourse.vue
+++ client/src/pages/AddCourse.vue
@@ -1,25 +1,57 @@
 <template>
-    <div>
-        <div>
-      <form >
-        <input  />
-        <button>Add Course to Catalog</button>
+    <div class="formWrapper">
+      <h1>Add a Course</h1>
+      <form @submit.prevent="handleSubmit">
+        <label>
+          Name:
+          <input type="text" :value="course.name" @input="course.name = $event.target.value" required />
+        </label>
+        <label>
+          Number:
+          <input type="text" :value="course.number" @input="course.number = $event.target.value" required />
+        </label>
+        <button type="submit">Submit</button>
       </form>
-   
-        
-      
+      <div v-if="message">
+        {{ message }}
+      </div>
     </div>
-    
-    </div>
-    </template>
-    
-    
-    <script>
-        export default { 
-            name: 'AddCourse',
+  </template>
+  
+  <script>
+  import axios from "axios";
+  
+  export default {
+    data() {
+      return {
+        course: {
+          name: "",
+          number: "",
+        },
+        message: "",
+      };
+    },
+    methods: {
+      async handleSubmit() {
+        try {
+          const response = await axios.post("'http://localhost:3001/api/course/createcourse", this.course);
+          if (response.status === 200) {
+            this.message = "Course has been added to the catalog!";
+            this.course = {
+              name: "",
+              number: "",
+            };
+          } else {
+            throw new Error("Error: Course was not added to catalog");
+          }
+        } catch (error) {
+          console.error(error);
+          this.message = "An error occurred while adding the course.";
         }
-    </script>
-    
+      },
+    },
+  };
+  </script>
     
     <style>
     
